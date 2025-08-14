c55f5d85e1dff89f9665f6ce9a11ca8f502afb95
Abhinav arya <aryaabhinav219@gmail.com>
2025-08-14 10:49:49 +0530

    Created server Files
    Created the env file for variables,app.js file for routing and database connection and server.js file for creating server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(morgan('dev'));
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Database Connected'))
+.catch(err => console.log('Database connection error:', err));
+
+
+
+module.exports = app;
