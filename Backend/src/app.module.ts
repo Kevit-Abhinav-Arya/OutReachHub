442e1800384f6690edc729afb6f8beca69169ba4
Abhinav <abhinav.arya@kevit.io>
2025-08-26 11:36:15 +0530

    History: commits from previous branch
    Contains commit from previous branch

---
+++ Backend/src/app.module.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { config } from './config/configuration';
+
+// Modules
+import { AuthModule } from './auth/auth.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { MessageTemplatesModule } from './message-templates/message-templates.module';
+import { CampaignsModule } from './campaigns/campaigns.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { DatabaseModule } from './database/database.module';
+
+// Filters
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+
+@Module({
+  imports: [
+    // Configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      envFilePath: '.env',
+    }),
+
+    // Feature Modules
+    DatabaseModule,
+    AuthModule,
+    WorkspacesModule,
+    UsersModule,
+    ContactsModule,
+    MessageTemplatesModule,
+    CampaignsModule,
+    AnalyticsModule,
+  ],
+  providers: [
+    // Global Exception Filter
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
+})
+export class AppModule {}
---
+++ Backend/src/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  // Implementation will be added in next phase
+}
