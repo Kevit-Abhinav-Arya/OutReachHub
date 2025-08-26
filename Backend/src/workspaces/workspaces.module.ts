b40589971a6ffe9f7c6c9a5fcfb779c331e41e21
Abhinav <abhinav.arya@kevit.io>
2025-08-26 18:21:22 +0530

    Feat: Workspace Logic
    Created the workspace logic in controller module and service files

--- Backend/src/workspaces/workspaces.module.ts
+++ Backend/src/workspaces/workspaces.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 import { WorkspacesController } from './workspaces.controller';
 import { WorkspacesService } from './workspaces.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
   exports: [WorkspacesService],
