5996042c3e3ecd6cd1ee3b6b427eda3d74783aa7
Abhinav <abhinav.arya@kevit.io>
2025-08-26 18:18:48 +0530

    Feat: Workspace Dto
    Created the Workspace Dtos

---
+++ Backend/src/workspaces/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './create-workspace.dto';
+export * from './update-workspace.dto';
+export * from './create-workspace-user.dto';
+export * from './update-workspace-user.dto';
+export * from './query.dto';
+export * from './response.dto';
