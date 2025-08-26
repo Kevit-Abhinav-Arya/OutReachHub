a4fc1807b032e5894e0c402156f7b1feb12cf60d
Abhinav <abhinav.arya@kevit.io>
2025-08-26 15:52:58 +0530

    History: Previous Commits
    This commit contains all the previous commits so that there is just only one commit for the previous commits instead of a large commit history

---
+++ Backend/src/message-templates/message-templates.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('message-templates')
+export class MessageTemplatesController {
+  // Implementation will be added in next phase
+}
---
+++ Backend/src/users/users.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  // Implementation will be added in next phase
+}
