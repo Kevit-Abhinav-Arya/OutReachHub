0e2828d11ebd6bca77885d8c12fee4e1837c4abf
Abhinav <abhinav.arya@kevit.io>
2025-08-27 14:36:26 +0530

    Feat: Contact Logic
    Created the contacts logic in the controller module and service files

--- Backend/src/contacts/contacts.module.ts
+++ Backend/src/contacts/contacts.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { CommonModule } from '../common/common.module';
+import { AuthModule } from '../auth/auth.module';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CommonModule, AuthModule],
   controllers: [ContactsController],
   providers: [ContactsService],
   exports: [ContactsService],
