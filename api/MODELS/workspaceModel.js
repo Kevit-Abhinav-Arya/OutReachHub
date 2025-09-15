985f2c7ebd5d5878711a03c593ab7be17568757b
Abhinav <abhinav.arya@kevit.io>
2025-09-15 12:33:02 +0530

    History: Previous Commits
    This commit contains all the previous commits so that there is just only one commit for the previous commits instead of a large commit history

--- api/MODELS/workspaceModel.js
+++ api/MODELS/workspaceModel.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const workspaceSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
-    createdAt: {type: Date, default: Date.now}
-
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Workspace',workspaceSchema);
+module.exports = mongoose.model("Workspace", workspaceSchema);
